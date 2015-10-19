474bde28d6b4d998b4cab2cc2dc2e56b1f482ad5
kingrichard2005@gmail.com <kingrichard2005@gmail.com>
2015-10-19 07:23:13 -0700

    * added test project for F-Sharp implementation of DigitsRecognizer

---
+++ DigitsRecognizer/DigitsRecognizerFS/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace DigitsRecognizerFS
+
+type Class1() = 
+    member this.X = "F#"
---
+++ DigitsRecognizer/DigitsRecognizerFS/Script.fsx
@@ -0,0 +1,29 @@
+﻿#load "Library1.fs"
+open DigitsRecognizerFS
+open System.IO
+open System
+
+// Define your library scripting code here
+// Testing
+let x = 42
+let y = x + 100
+
+type Observation = { Label:string; Pixels: int[] }
+
+let toObservation (csvData:string) =
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let baseDirectory = __SOURCE_DIRECTORY__
+let baseDirectory' = Directory.GetParent(baseDirectory)
+let filePath = @"DigitsRecognizerTests\datasamples\digits\trainingsample.csv"
+let fullPath = Path.Combine(baseDirectory'.FullName, filePath)
+let trainingPath = fullPath
+let trainingData = reader trainingPath
